1782977ed6729223422c8d76532e485fb3668c49
Daminiofficial99 <89516841+Daminiofficial99@users.noreply.github.com>
2021-08-25 17:03:59 +0530

    Update first.py

--- first.py
+++ first.py
@@ -11,18 +11,18 @@ import time
 #st.line_chart(chart_data)
 
 
- map_data=pd.DataFrame(
-     np.random.randn(1000,2)/[50,50]+[37.76,-122.4],
-     columns=['lat','lon'])
- st.map(map_data)
+ #map_data=pd.DataFrame(
+  #   np.random.randn(1000,2)/[50,50]+[37.76,-122.4],
+   #  columns=['lat','lon'])
+ #st.map(map_data)
 
 
 
-# if st.checkbox('show dataframe'):
-#     chart_data=pd.DataFrame(
-#         np.random.randn(20,3),
-#         columns=['a','b','c'])
-#     chart_data
+ if st.checkbox('show dataframe'):
+     chart_data=pd.DataFrame(
+       np.random.randn(20,3),
+         columns=['a','b','c'])
+     chart_data
 
 
 # option=st.selectbox(
